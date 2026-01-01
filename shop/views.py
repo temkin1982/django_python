998d13a4893c75c4ccdd7a9fad9495276ac7fce6
Dima Temkin <temkindima81@gmail.com>
2026-01-01 13:36:24 +0200

    edit html and css

--- shop/views.py
+++ shop/views.py
@@ -5,4 +5,5 @@ from .models import Course
 # Create your views here.
 def index(request):
     courses = Course.objects.all()
-    return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
+    # return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
+    return render(request, 'courses.html', {'courses': courses})
